df191f4cae5b01b60b279d23fe98ba9c7ed8cc7f
Daniel Zych <danjzych@gmail.com>
2024-06-23 18:57:51 -0500

    Added first react hook. Simply useXY to layout development pattern before getting further

---
+++ packages/react/src/useXY/useXY.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+interface MousePosition {
+  x: number;
+  y: number;
+}
+
+/**
+ * Convenience utility hook for tracking client cursor position.
+ */
+const useXY = () => {
+  const [pos, setPos] = useState<MousePosition>({ x: 0, y: 0 });
+  const updatePos = (e: MouseEvent): void => {
+    setPos({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", updatePos);
+
+    return window.removeEventListener("mousemove", updatePos);
+  });
+
+  return {
+    x: pos.x,
+    y: pos.y,
+  };
+};
+
+export default useXY;

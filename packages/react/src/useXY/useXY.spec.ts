622cf94d50f77e08051a1e890db917e8414336c3
Daniel Zych <danjzych@gmail.com>
2024-06-29 21:00:08 -0500

    Further test and dependency configuration, added test

--- packages/react/src/useXY/useXY.spec.ts
+++ packages/react/src/useXY/useXY.spec.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import { renderHook } from '@testing-library/react';
+import {userEvent} from '@testing-library/user-event'
 import useXY from './useXY';
 
 describe('useXY', () => {
@@ -7,4 +8,11 @@ describe('useXY', () => {
 		const { result } = renderHook(useXY);
 		expect(result.current).toEqual({ x: 0, y: 0 });
 	});
+	it('should update x and y when cursor is moved', async () => {
+		const user = userEvent.setup()
+		const { result, rerender } = renderHook(useXY);
+		await user.pointer({coords: {x: 10, y: 10}})
+		rerender()
+		expect(result.current).toEqual({x: 10, y: 10})
+	})
 });
